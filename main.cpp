9e06a37b07b098f75ef720f5c8fca6d8e1d1eb2a
RealMasda12 <kapceroski@gmail.com>
2026-01-28 17:24:57 +0100

    02 Zmiana mnożenia w  metodzie Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int Policz(int a,int b) {
-    return a+b;
+    return a*b;
 }
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
